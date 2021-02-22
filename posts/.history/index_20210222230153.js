e71f13c4f87c8a2238bcb6f7b55a456b836c76b1
Chiranjivee Saksham <chiranjivee960@gmail.com>
2021-02-23 04:50:16 +0530

    init

---
+++ posts/.history/index_20210222230153.js
@@ -0,0 +1,14 @@
+const express=require('express')
+const app=express()
+
+app.get('/posts',(req,res)=>{
+
+})
+
+app.post('/posts',(req,res)=>{
+
+})
+
+app.listen(4000,()=>{
+    console.log('server is up on port 4000')
+})
